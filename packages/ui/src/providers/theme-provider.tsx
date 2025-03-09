78b857a0fe2aabe9991f43d6b7a0276faf860ba0
angeloaruta <arutaangelo@gmail.com>
2025-03-09 18:29:07 -0400

    add speed insights

--- packages/ui/src/providers/theme-provider.tsx
+++ packages/ui/src/providers/theme-provider.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import * as React from "react"
-
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
@@ -13,6 +13,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
       enableColorScheme
     >
       {children}
+      <SpeedInsights />
     </NextThemesProvider>
   )
 }
