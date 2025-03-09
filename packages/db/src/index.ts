e8c52cde2bba948bfee3ce741ca4a366467b6ff8
angeloaruta <arutaangelo@gmail.com>
2025-03-09 17:59:08 -0400

    fix db

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/postgres-js"
+import { env } from "@recharge/utils/env"
 import postgres from "postgres"
 
 import * as schema from "./schema"
@@ -11,7 +12,7 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined
 }
 
-const conn = globalForDb.conn ?? postgres(process.env.DATABASE_URL!)
-if (process.env.NODE_ENV !== "production") globalForDb.conn = conn
+const conn = globalForDb.conn ?? postgres(env.DATABASE_URL)
+if (env.NODE_ENV !== "production") globalForDb.conn = conn
 
 export const db = drizzle(conn, { schema })
