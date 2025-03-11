78085f84f64aa0d880422aadf8470e5215c53c54
angeloaruta <arutaangelo@gmail.com>
2025-03-11 09:18:44 -0400

    clean up admin layout

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,47 +1,16 @@
 import { ThemeProvider } from "@recharge/ui/providers/theme-provider"
 import { cn } from "@recharge/ui/lib/utils"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-
 import "@recharge/ui/globals.css"
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Recharge App",
-  description: "Recharge Your Health at Home",
+  title: "Recharge Admin",
+  description: "Recharge Admin",
   icons: {
     icon: "/favicon.ico",
   },
-  applicationName: "Recharge App",
-  creator: "Recharge App",
-  category: "Healthcare",
-  keywords: [
-    "Recharge",
-    "Physical Therapy",
-    "Home Healthcare",
-    "Rehabilitation",
-    "Telehealth",
-    "Exercise Programs",
-    "Recovery",
-    "Home Exercises",
-    "PT Exercises",
-    "Pain Management",
-    "Injury Recovery",
-    "Remote Physical Therapy",
-    "Mobility",
-    "Strength Training",
-    "Flexibility",
-    "Healthcare Technology",
-    "Wellness",
-    "Rehabilitation Services",
-    "Home Rehabilitation",
-    "Physical Health",
-    "Movement Therapy",
-    "Healthcare App",
-    "Therapeutic Exercise",
-    "Patient Care",
-    "Digital Healthcare",
-  ],
+  applicationName: "Recharge Admin",
+  creator: "Recharge Admin",
 }
 
 export default function RootLayout({
@@ -51,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("antialiased", GeistSans.className, GeistMono.className)}>
+      <body className={cn("antialiased")}>
         <div className="bg-background-200 min-h-vh relative">
           <main>
             <ThemeProvider>{children}</ThemeProvider>
